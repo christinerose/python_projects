f18b7278457cb3294941946cc379a2494dbf7c80
Christine Rose <christinerose@Christines-MacBook-Air.local>
2021-02-17 13:25:58 -0800

    New Python practice program: MadLibs

---
+++ MadLibs.py
@@ -0,0 +1,13 @@
+noun = input("Give me a noun: ")
+noun2 = input("Give me another noun: ")
+celebrity = input("Give me celebrity's name: ")
+verb = input("Give me an action verb: ")
+verbing = input("Give me an -ing verb: ")
+animal = input("Name an animal that starts with a consonant: ")
+
+print("That", noun, "is singing to a big", noun2 + "!")
+print("I want to", verb, "with you all day!")
+print("Did I just catch you", verbing + "?")
+print("Last night, I saw", celebrity, "riding a", animal + "!")
+
+
