0efdc5fae0b02d3f92a2d441059f000640a48b48
Christine Rose <professor.rose@gmail.com>
2021-03-01 08:45:09 -0800

    Adding Sets & Dictionary HW from Pirple

---
+++ key&dict.py
@@ -0,0 +1,20 @@
+print(" ")
+print("*******")
+print(" ")
+
+Favorite_Song = {
+    "Artist": "Duran Duran",
+    "Song": "Hold Back the Rain",
+    "Genre": "New Romantics",
+    "Year": "1982",
+    "Length in Minutes": "3.25",
+    "Bass Line": "Wicked"
+}
+
+for key, value in Favorite_Song.items():
+    print(key, ':', value)
+
+
+print(" ")
+print("*******")
+print(" ")
