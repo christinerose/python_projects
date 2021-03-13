91157393488d786b590887ed508b5e7073f75283
Christine Rose <professor.rose@gmail.com>
2021-03-13 10:01:48 -0800

    Updated ROT, still having trouble

--- ROT.py
+++ ROT.py
@@ -2,28 +2,42 @@ import ROT
 
 enigma_string = "It's fun to deciper a coded message!"
 
+print(" ")
+print("**********")
+print(" ")
+
 
 # having a difficult time with this. Progress:
 
 def rot13():
 
     rot_message = input(
-        "Enter a word or p hrase to create your secret message: ")
+        "Enter a word or phrase to create your secret message: ")
 
     word_given = ['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']
 
     word_output = ['NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijkklm']
     # list like this or dictionary? I'm not sure what to do here.
 
+    lenth = len(rot_message)
+    print(lenth)
+
     for letter in rot_message:
-        while letter <= len(rot_message):
-            print(letter)
+
+        for match in (word_given):
+            if match == letter:
+                for position in range(int(word_given)):
+                    print(position)
 
     print(" ")
     print("**********")
     print(" ")
 
     print(rot_message)
+
+    print(" ")
+    print("**********")
+    print(" ")
 
 
 rot13()
