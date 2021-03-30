4347a12a4248a26ae1e37fa7c5086f0f40be8d8e
Christine Rose <professor.rose@gmail.com>
2021-03-29 18:19:23 -0700

    Struggling with this write/read file assignment

---
+++ io_hw.py
@@ -0,0 +1,71 @@
+# Create a note-taking program. When a user starts it up, it should 
+# prompt them for a filename.
+
+# If they enter a file name that doesn't exist, it should prompt 
+# them to enter the text they want to write to the file. After they 
+# enter the text, it should save the file and exit.
+
+# If they enter a file name that already exists, it should ask 
+# the user if they want:
+
+# A) Read the file
+# B) Delete the file and start over
+# C) Append the file
+
+# If the user wants to read the file it should simply show the 
+# contents of the file on the screen. If the user wants to start 
+# over then the file should be deleted and another empty one made 
+# in its place. If a user elects to append the file, then they 
+# should be able to enter more text, and that text should be 
+# added to the existing text in the file. 
+
+print(" ")
+print("***********")
+print(" ")
+
+
+fileName = []
+count = 0
+
+print(fileName)
+print(" ")
+
+while count <= 2:
+    chooseName = input("Please choose the file you'd like to open: ")
+
+    # elem in fileName(  # if True... ask to choose another)
+
+    if chooseName in fileName:
+        print("That file exists! \n")
+        choiceAction = input(
+            "Would you like to: \nA) Read the file\nB) Delete the file and start over\nC) Append the file. ")
+
+        if choiceAction == A or a:  # read, show contents
+            chooseName = open("fileName.txt", "r")
+            print(chooseName)
+            print("You typed A")
+
+        elif choiceAction == B or b:  # delete file and put another empty one in its place
+            chooseName.delete  # ????
+            print("File deleted")
+            print(" ")
+
+        # elif choiceAction == C or c:  # enter more text to append file
+        #     chooseName.append
+        #     print("File appended")
+        #     print(" ")
+    else:
+        print("That file doesn't exist. What would you like to write in the file?")
+        tempFile = []
+        contents = (input("Type new contents here: "))
+        tempFile.append(contents)
+
+        fileName.append(chooseName)
+
+        count += 1
+
+print(" ")
+print("***********")
+print(" ")
+
+print(fileName)
